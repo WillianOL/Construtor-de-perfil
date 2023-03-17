a15768ef19870c525ba34be734ee2cb88020809e
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-03-17 18:46:22 -0300

    Atualizações

--- assets/index.js
+++ assets/index.js
@@ -1,22 +1,32 @@
-const modal = document.querySelector('.conteinerModal')
-const btnGerarPefil = document.querySelector('#btn_JS'); 
+const modal = document.querySelector('.conteinerModal');
+const btnGerarPefil = document.querySelector('#btn_JS');
+const inputName = document.getElementById('idNome');
+const inputAnoNascimento = document.getElementById('idAnoNas');
+const nameOfPersonDiv = document.querySelector('.nome');
 
 function perfilConstructor() {
-    const nome = document.getElementById('idNome').value;
-    const anoNasc = document.getElementById('idAnoNas').value;
-    
-    if(anoNasc.length === 0 || anoNasc <= 0 || nome.length === 0){
-        modal.classList.add('modalAtivo')
+    const nome = inputName.value;
+    const anoNascimento = inputAnoNascimento.value;
+
+    if(anoNascimento.length === 0 || anoNascimento <= 0 || nome.length === 0){
+        modal.classList.add('abreModal');
     } else{
         const date = new Date();
-        const ano = date.getFullYear()
+        const anoAtual = date.getFullYear();
+        const resultadoIdade = anoAtual - anoNascimento;
+        const imagemPerfil = document.querySelector('.fotoPerfil img');
+        const inputGenero = document.querySelectorAll('.sexDado input');
+
         
+        if(inputGenero[0].checked){
+
+        }
     }
 }
 
 btnGerarPefil.addEventListener('click', perfilConstructor);
 
-const btnModal = document.querySelector('.btnModal')
+const btnModal = document.querySelector('.btnModal');
 btnModal.addEventListener('click', () => {
-    modal.classList.remove('modalAtivo')
+    modal.classList.remove('abreModal');
 })
